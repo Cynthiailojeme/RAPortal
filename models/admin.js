9da1d70d0d172313cab37ca3320166e78744fc8a
Cynthiailojeme <cynthiaezinne1@gmail.com>
2019-09-24 09:29:25 +0100

    admin and user

---
+++ models/admin.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const adminSchema = mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password:{
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
