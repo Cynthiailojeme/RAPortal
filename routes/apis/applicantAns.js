852fe16dfff949ad892b6481bb4308476cc5b337
Abass Adamo <adamoabasstope@yahoo.com>
2019-10-10 10:50:40 +0100

    commit changes

--- routes/apis/applicantAns.js
+++ routes/apis/applicantAns.js
@@ -6,73 +6,107 @@ const TokenMiddleware = require('../../middleware/token');
 
 
 router.post("/send", TokenMiddleware, (req, res, next) => {
-      const answers = req.body.answers;
-      const setId = req.body.setId;
-      const { userId } = req.decoded;
-      let score = 0;
+  const answers = req.body.answers;
+  const setId = req.body.setId;
+  const { userId } = req.decoded;
+  const takenassessment = req.body.takenassessment
+  let score = 0;
+
+
 
   QuestionSet.findById(setId)
-      .then((questionset) => {
-        // console.log(questionset.quiz)
-        for(var j = 0; j < answers.length; j++) {
-          for(var k = 0; k < questionset.quiz.length; k++) {
-            console.log(answers[j].question_id, questionset.quiz[k]._id)
-            if(answers[j].question_id == questionset.quiz[k]._id) {
-              console.log(answers[j].ans, questionset.quiz[k].correctAnswer)
-              if(answers[j].ans === questionset.quiz[k].correctAnswer){
-                score+=1;
-              }
-              // console.log(questionset.quiz[k])
+    .then((questionset) => {
+      // console.log(questionset.quiz)
+      for (var j = 0; j < answers.length; j++) {
+        for (var k = 0; k < questionset.quiz.length; k++) {
+          console.log(answers[j].question_id, questionset.quiz[k]._id)
+          if (answers[j].question_id == questionset.quiz[k]._id) {
+            console.log(answers[j].ans, questionset.quiz[k].correctAnswer)
+            if (answers[j].ans === questionset.quiz[k].correctAnswer) {
+              score += 1;
             }
+            // console.log(questionset.quiz[k])
           }
         }
-        newApplicantAns = new ApplicantAns({
-          answers: answers,
-          setId: setId,
-          score: score,
-          userId: userId,
-          timestamps: true
+      }
+
+      newApplicantAns = new ApplicantAns({
+        answers: answers,
+        setId: setId,
+        score: score,
+        userId: userId,
+        timestamps: true,
+        takenassessment,
       });
-      newApplicantAns.save()
-      .then(applicantans => {
-          res.status(200).json({
-            message: "Uploaded successfully",
-            applicantans
-          }); 
-      })
-      .catch(err => console.log(err));
-      console.log(score)
-          // res.json(questionset);
-      })
-      .catch(err => console.log(err))
-        
-      });
+
+
+      ApplicantAns.findById(userId).then((result) => {
+        if(result == undefined || result.length == 0){
+          res.status(400).json({
+            message: "No answer submitted yet"
+          });
+        }else{
+          res.status(400).json({
+            message: "User Already submitted assessment"
+          });
+        }
+        }).catch((err) => {
+          newApplicantAns.save()
+            .then(applicantans => {
+              res.status(200).json({
+                message: "Uploaded successfully",
+                applicantans
+              });
+            })
+            .catch(err => console.log(err));
+          console.log(score)
+          console.log(err)
+        })
+
+
+    })
+    .catch(err => console.log(err))
+});
+
+// delete at once all collections in the db
+router.delete('/empty', (req, res, next) => {
+  ApplicantAns.remove({}, function (err) {
+    if (err) {
+      console.log(err)
+      res.json({ status: "Error Deleting all documents" });
+    } else {
+      res.json({ status: "Successfully deleted all documents in collection" });
+    }
+  }
+  );
+})
 
 // Get all users tests scores
 router.get('/all', (req, res, next) => {
   ApplicantAns.find()
-      .then((applicantanss) => {
-          res.json(applicantanss);
-      })
-      .catch(err => console.log(err))
+    .then((applicantanss) => {
+      res.json(applicantanss);
+    })
+    .catch(err => console.log(err))
 });
 
-// make delete a applicantans
+// make delete a applicant ans
 router.delete('/:id', (req, res, next) => {
   let id = req.params.id;
   ApplicantAns.findById(id)
-  .then(applicantans => {
-    applicantans.delete()
-      .then(applicantans =>{
-          res.send({message: 'Question deleted succesfully',
-          status: 'success',
-          applicantans: applicantans})
+    .then(applicantans => {
+      applicantans.delete()
+        .then(applicantans => {
+          res.send({
+            message: 'Question deleted succesfully',
+            status: 'success',
+            applicantans: applicantans
+          })
 
-      })
-      .catch(err => console.log(err))
-  })
-  .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
 })
 
 module.exports = router;
-  
--- routes/apis/questionSet.js
+++ routes/apis/questionSet.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const QuestionSet = require('../../models/QuestionSet');
+const TokenMiddleware = require('../../middleware/token');
+const ApplicantAns = require('../../models/ApplicantAns');
 
 // Create a Questionset
 router.post('/add', (req, res, next) => {
@@ -9,13 +11,15 @@ router.post('/add', (req, res, next) => {
       const quiz = req.body.quiz;
       const duration = req.body.duration;
       const dateOfAsess = req.body.dateOfAsess;
+      
   
       newQuestionSet = new QuestionSet({
-        nameOfSet: nameOfSet,
+          nameOfSet: nameOfSet,
           quiz: quiz,
           duration: duration,
           dateOfAsess: dateOfAsess,
-          timestamps: true
+          timestamps: true,
+          
       });
       newQuestionSet.save()
       .then(questionset => {
@@ -28,13 +32,21 @@ router.post('/add', (req, res, next) => {
   });
 
   // Get one questionset
-router.get('/single/:id', (req, res, next) => {
+router.get('/single/:id', TokenMiddleware, (req, res, next) => {
+  const {userId} = req.decoded;
+  console.log( "user id " + userId)
   //Grab the id of the questionset
   let id = req.params.id;
-  QuestionSet.findById(id)
-      .then((questionset) => {
-        console.log(questionset)
-          res.json(questionset);
+  ApplicantAns.find({userId})
+      .then((response) => {
+        QuestionSet.findById(id)
+        .then((questionset) => {
+          console.log(questionset)
+            res.json(questionset);
+        })
+        .catch(err => console.log(err))
+        console.log(response)
+          // res.json(questionset);
       })
       .catch(err => console.log(err))
 });
